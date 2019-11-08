6cd724336d948ce94ad019abef6648359b792cdb
ganyeyu <446197219@qq.com>
2019-11-08 22:50:54 +0800

    1

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 console.log("hello git master 1 222 issue-001");
 console.log("");
+=======
+console.log("hello git master 1 222");
+console.log(" dev ");
+>>>>>>> Stashed changes
